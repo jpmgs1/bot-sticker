43d53b36ebc2ceec02723eed7516450929ca1f97
jpmgs1 <79082126+jpmgs1@users.noreply.github.com>
2021-02-26 21:00:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ async function connectToWhatsApp() {
             `-vcodec`,
             `libwebp`,
             `-vf`,
-            `crop=w= 0|'min(min(iw\,ih)\,500)':h='min(min(iw\,ih)\,500)',scale=500:500,setsar=1,fps=${processOptions.fps}`s}`,
+            `crop=w='min(min(iw\,ih)\,500)':h='min(min(iw\,ih)\,500)',scale=500:500:force_original_aspect_ratio=decrease,setsar=1,fps=${processOptions.fps}`,
             `-loop`,
             `${processOptions.loop}`,
             `-ss`,
